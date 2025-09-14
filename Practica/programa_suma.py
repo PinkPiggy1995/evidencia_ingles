028083dbc6204668fbbd86a0b4c717b436617aed
PinkPiggy <vaci.ispc@gmail.com>
2025-09-13 21:07:26 -0300

    add num4

--- Practica/programa_suma.py
+++ Practica/programa_suma.py
@@ -1,10 +1,11 @@
 num1 = int(input("Ingrese el primer número:"))
 num2 = int(input("Ingrese el segundo número:"))
 num3 = int(input("Ingrese el tercer número:"))
+num4 = int(input("Ingrese el cuarto número:"))
 
-suma = num1 + num2 + num3
+suma = num1 + num2 + num3 + num4
 
-promedio = suma/3
+promedio = suma/4
 
 print("La suma de los tres números es: ", suma, "y su promedio da: ", promedio)
 
